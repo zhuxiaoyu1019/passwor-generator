b6baf5618da60245bef68163c6bebda2f6eb666e
RitaZ <xiaoyz109@gmail.com>
2020-12-10 19:44:26 -0800

    display message when user not select any checkbox

--- script.js
+++ script.js
@@ -61,14 +61,17 @@ function generatePassword(passwordLength, hasSymbols, hasNum, hasLowerCase, hasU
     arrCodes = arrCodes.concat(UPPERCASE_CODES);
   }
 
-  //generate password 
-  var password = [];
-  for (var i = 0; i < passwordLength; i++) {
-    var randChar = arrCodes[Math.floor(Math.random() * arrCodes.length)];
-    password.push(String.fromCharCode(randChar));
+  if (arrCodes.length === 0) {
+    return "Please select at least one type of the character"
+  } else {
+    //generate password 
+    var password = [];
+    for (var i = 0; i < passwordLength; i++) {
+      var randChar = arrCodes[Math.floor(Math.random() * arrCodes.length)];
+      password.push(String.fromCharCode(randChar));
+    }
+    return password.join("");
   }
-
-  return password.join("");
 }
 
 function arrChar(low, high) {
@@ -81,25 +84,25 @@ function arrChar(low, high) {
 
 
 /** ------------------------------ Helper Functions  ------------------------------ */
-  /**
-   * Note: You may use these in your code, but remember that your code should not have
-   * unused functions. Remove this comment in your own code.
-   */
+/**
+ * Note: You may use these in your code, but remember that your code should not have
+ * unused functions. Remove this comment in your own code.
+ */
 
-  /**
-   * Returns the element that has the ID attribute with the specified value.
-   * @param {string} idName - element ID
-   * @returns {object} DOM object associated with id.
-   */
-  function id(idName) {
-    return document.getElementById(idName);
-  }
+/**
+ * Returns the element that has the ID attribute with the specified value.
+ * @param {string} idName - element ID
+ * @returns {object} DOM object associated with id.
+ */
+function id(idName) {
+  return document.getElementById(idName);
+}
 
-  /**
-   * Returns the first element that matches the given CSS selector.
-   * @param {string} selector - CSS query selector.
-   * @returns {object} The first DOM object matching the query.
-   */
-  function qs(selector) {
-    return document.querySelector(selector);
-  }
+/**
+ * Returns the first element that matches the given CSS selector.
+ * @param {string} selector - CSS query selector.
+ * @returns {object} The first DOM object matching the query.
+ */
+function qs(selector) {
+  return document.querySelector(selector);
+}
